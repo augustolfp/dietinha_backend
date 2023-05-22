ca6ae012539be1221d57862ccb39298d5f5bbb9d
augustolfp <augustolfp@gmail.com>
2023-05-22 19:06:57 -0300

    feat(Database): implement postgres db with prisma and docker

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM node:18.16.0
 
 WORKDIR /app
 
-EXPOSE 3000
+EXPOSE 3001
 
 CMD ["npm","run","dev:container"]
--- src/repositories/tacoTableRepository.ts
+++ src/repositories/tacoTableRepository.ts
@@ -1,9 +1,13 @@
+import { prisma } from "../config/database";
+
 export async function search(term: string) {
     if (term.length < 3) {
         throw Error("Termo de pesquisa inválido");
     }
 
-    return {
-        result: `Search term: ${term}`,
-    };
+    return await prisma.$queryRaw`
+    SELECT *
+    FROM "tacoTable"
+    WHERE
+      description ILIKE ${"%" + term + "%"}`;
 }
