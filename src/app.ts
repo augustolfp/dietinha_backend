f993098203cbd5f0727cab3508e845d40a6635a5
augustolfp <augustolfp@gmail.com>
2023-08-18 19:21:33 -0300

    docs: configure swaggerUI for api docs

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import "express-async-errors";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
 import router from "./routes";
 import errorHandlerMW from "./middlewares/errorHandlerMW";
+import swaggerDocs from "./swagger.json";
 
 const app = express();
+
 app.use(express.json(), cors());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(router);
 app.use(errorHandlerMW);
 
