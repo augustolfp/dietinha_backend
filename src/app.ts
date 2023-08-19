2336a3822b4534d9f8f62d7d40439c8506a47887
augustolfp <augustolfp@gmail.com>
2023-08-19 13:01:17 -0300

    refactor: move openapi JSON file to projects root dir

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 import swaggerUi from "swagger-ui-express";
 import router from "./routes";
 import errorHandlerMW from "./middlewares/errorHandlerMW";
-import swaggerDocs from "./swagger.json";
+import swaggerDocs from "../swagger.json";
 
 const app = express();
 
